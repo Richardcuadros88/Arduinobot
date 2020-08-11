c325fb8aa2d33a8104ef836c1c04c55523079207
Richardcuadros88 <richardct8@gmail.com>
2020-08-11 10:11:08 -0500

    node

---
+++ ArduinoChatBot_Nodejs/index.js
@@ -0,0 +1,26 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+
+const token = '1093970624:AAGv5-k6E53uApfVZYPwalgcE73Z5E14Ud8';
+
+
+const bot = new TelegramBot(token, {polling: true});
+
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+
+
+  const chatId = msg.chat.id;
+  const resp = match[1];
+
+
+  bot.sendMessage(chatId, resp);
+});
+
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+
+  bot.sendMessage(chatId, 'Received your message');
+});
