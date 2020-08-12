d7024611b68b7736247d4daa55dbace90be782d8
Richardcuadros88 <richardct8@gmail.com>
2020-08-12 00:14:59 -0500

    chatid

--- ArduinoChatBot_Nodejs/index.js
+++ ArduinoChatBot_Nodejs/index.js
@@ -5,19 +5,26 @@ const token = '1093970624:AAGv5-k6E53uApfVZYPwalgcE73Z5E14Ud8';
 
 const bot = new TelegramBot(token, {polling: true});
 
-
+var SerialPort = require('serialport')
+var Mipuerto = new SerialPort('/dev/tty-COM4', {
+  baudRate: 9600,
+  autoOpen: true
+})
 
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
+  console.log("El ID del char" + chatId);
   var Mensaje = msg.text;
-  if (Mensaje == "encender"){
+  if (Mensaje == "Encender"){
     console.log("encender el led")
       bot.sendMessage(chatId, 'encendere el led ');
+      Mipuerto.write("H");
     }
-    else if(Mensaje == "apagar"){
+    else if(Mensaje == "Apagar"){
     console.log("apagar led");
       bot.sendMessage(chatId, 'apagare el led ');
+      Mipuerto.write("L");
     }
   bot.sendMessage(chatId, 'Recibí tu mensaje ');
 });
