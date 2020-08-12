d32c6381913e4ba538b2838c759bc1906e1da3b0
Richardcuadros88 <richardct8@gmail.com>
2020-08-11 21:47:21 -0500

    codigo telegram

--- ArduinoChatBot_Nodejs/index.js
+++ ArduinoChatBot_Nodejs/index.js
@@ -7,20 +7,18 @@ const token = '1093970624:AAGv5-k6E53uApfVZYPwalgcE73Z5E14Ud8';
 const bot = new TelegramBot(token, {polling: true});
 
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
-
-
-  const chatId = msg.chat.id;
-  const resp = match[1];
-
-
-  bot.sendMessage(chatId, resp);
 });
-
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-
-
+  var Mensaje = msg.text;
+  if (Mensaje == "encender"){
+    console.log("encender el led")
+      bot.sendMessage(chatId, 'encendere el led ');
+    }
+    else if{Mensaje == "apagar"){
+    console.log("apagar led");
+      bot.sendMessage(chatId, 'apagare el led ');
+    }
   bot.sendMessage(chatId, 'Recibí tu mensaje ');
 });
