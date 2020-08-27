9b7d70797430a9f66c79e4e45573c8cbc0aab5d2
Richardcuadros88 <richardct8@gmail.com>
2020-08-27 06:23:45 -0500

    espp mqtt

--- ArduinoChatBot_Nodejs/index.js
+++ ArduinoChatBot_Nodejs/index.js
@@ -1,24 +1,27 @@
 
 const TelegramBot = require('node-telegram-bot-api');
+var mqtt = require('mqtt')
 const token = '1093970624:AAGv5-k6E53uApfVZYPwalgcE73Z5E14Ud8';
+//const token = '1093970624:AAGv5-k6E53uApfVZYPwalgcE73Z5E14Ud8';
 const bot = new TelegramBot(token, {
   polling: true
 });
 var IdMiChat = 1189408516;
-var SerialPort = require('serialport');
-var MiPuerto = new SerialPort('COM4', {
-  baudRate: 9600,
-  autoOpen: true
-});
+//var SerialPort = require('serialport');
+//var MiPuerto = new SerialPort('COM4', {
+  //baudRate: 9600,
+  //autoOpen: true
+var client = mqtt.connect('mqtt://housefox:123andres@broker.shiftr.io')
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   console.log("El ID del char" + chatId);
-  var Mensaje = msg.text;
-  if (Mensaje == "Encender") {
-    console.log("Encendiendo el led");
-    bot.sendMessage(chatId, 'Encendere el led');
-    MiPuerto.write("H");
+  var Mensaje = msg.text.toString().toLocaleLowerCase();
+  if (Mensaje.indexOf("encender") >= 0) {
+    console.log("encendiendo el led");
+     bot.sendMessage(chatId, 'Encendere el led');
+    client.publish('/fH/foco/casa', '0')
+    //MiPuerto.write("H");
   } else if (Mensaje == "Apagar") {
     console.log("Apagar el led");
     bot.sendMessage(chatId, 'Apagare el led');
@@ -28,12 +31,12 @@ bot.on('message', (msg) => {
 
 });
 
-MiPuerto.setEncoding('utf8');
+//MiPuerto.setEncoding('utf8');
 
-MiPuerto.on('data', function(data) {
-  console.log("Lo que entro es " + data);
-  if (data[0] == 'H') {
-    console.log("Boton Precionado");
-    bot.sendMessage(IdMiChat, "Se preciono el boton");
-  }
-});
+//MiPuerto.on('data', function(data) {
+  //console.log("Lo que entro es " + data);
+  //if (data[0] == 'H') {
+    //console.log("Boton Precionado");
+  //  bot.sendMessage(IdMiChat, "Se preciono el boton");
+//  }
+//});
