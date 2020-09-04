41ef2fa87b4454facb789fce0e79eaf72ca810b1
Richardcuadros88 <richardct8@gmail.com>
2020-09-03 22:45:34 -0500

    hyy

--- ArduinoChatBot_Arduino/ChatBotIOT/ChatBotIOT/ChatBotIOT.ino
+++ ArduinoChatBot_Arduino/ChatBotIOT/ChatBotIOT/ChatBotIOT.ino
@@ -22,9 +22,28 @@ MQTTClient client;
 
 int Led = 3;
 int Foco = 2;
+int estatus1 =0;
+int estatus2 =0;
 
-unsigned long lastMillis = 0;
 
+void setup() {
+  Serial.begin(115200);
+  pinMode(Led, OUTPUT);
+  pinMode(Foco, OUTPUT);
+  digitalWrite(Led, HIGH);
+  digitalWrite(Foco, HIGH);
+
+  Serial.println("Iniciando Wifi");
+  WiFi.mode(WIFI_STA);//Cambiar modo del Wi-Fi
+  delay(100);
+  wifiMulti.addAP(ssid1, pass1);
+  //wifiMulti.addAP(ssid2, pass2);
+ // wifiMulti.addAP(ssid3, pass3);
+
+ client.begin("broker.shiftr.io", net);
+  client.onMessage(RecibirMensaje);
+ // client.onMessage(RecibirMensaje2);
+ }
 void connect() {
   Serial.print("Conectando con Wifi...");
   while (wifiMulti.run() != WL_CONNECTED) {
@@ -44,7 +63,7 @@ void connect() {
  Led =  client.subscribe("/fs/sala/casa");
 
 }
- /*
+ 
 void RecibirMensaje(String &topic, String &payload) 
 
 {
@@ -52,11 +71,11 @@ void RecibirMensaje(String &topic, String &payload)
   Serial.println("Mensaje: " + topic + " - " + payload);
   
   if (payload == "1") {
-    digitalWrite(Foco, 1);
+    digitalWrite(Led, 1);
      
   //if (payload == "1")
    // digitalWrite(Led,1);
-   // Serial.println("iluminacion Activada");
+    Serial.println("iluminacion Activada");
   }
   else {
     digitalWrite(Foco, 0);
@@ -64,41 +83,6 @@ void RecibirMensaje(String &topic, String &payload)
     Serial.println("iluminacion Desactivada");
   }}
 
-*/
-
-  void RecibirMensaje2(String &topic, String &payload) 
- {
- Serial.println("Mensaje: " + topic + " - " + payload);
- if (payload == "0") {
-    digitalWrite(Led, 1);
-    Serial.println("iluminacion Activada2");
-  }
-  else {
-    digitalWrite(Led, 0);
-   Serial.println("iluminacion Desactivada2");
-  }
-}
-
-void setup() {
-  Serial.begin(115200);
-  pinMode(Led, OUTPUT);
-  pinMode(Foco, OUTPUT);
-  digitalWrite(Led, 0);
-  digitalWrite(Foco, 0);
-
-  Serial.println("Iniciando Wifi");
-  WiFi.mode(WIFI_STA);//Cambiar modo del Wi-Fi
-  delay(100);
-  wifiMulti.addAP(ssid1, pass1);
-  //wifiMulti.addAP(ssid2, pass2);
- // wifiMulti.addAP(ssid3, pass3);
-
-  client.begin("broker.shiftr.io", net);
-//  client.onMessage(RecibirMensaje);
-  client.onMessage(RecibirMensaje2);
-
-  connect();
-}
 
 void loop() {
   client.loop();
